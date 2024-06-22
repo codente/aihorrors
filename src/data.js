6557e0747997c014c6fdf9156a78d0614e63dafa
Jamie <jmeleacc@gmail.com>
2024-06-21 21:18:46 -0500

    fix text

--- src/data.js
+++ src/data.js
@@ -5,11 +5,11 @@ export const data = [
 	
   },
   {
-	 "alt": "An AI generated logo that contains extra content in the logo. The prompt was a logo for a plumbing business named A1 plumbing.",
+	 "alt": "An AI generated logo that contains extra text and misspellings in the text. The prompt was a logo for a plumbing business named A1 plumbing.",
 	"img": "aihorror2.webp",
 	},
      {
-	 "alt": "An AI generated logo that adds extra text to the logo. The prompt was a logo for a construction company named A1 construction.",
+	 "alt": "An AI generated logo that just fails with the text in the logo. The prompt was a logo for a construction company named A1 construction.",
 	"img": "aihorror3.webp",
 	}
  ]
