e9942fec50ee4958fa9a608ee024f28bc1629a79
Jamie <jmeleacc@gmail.com>
2024-06-21 21:13:20 -0500

    more images

--- src/data.js
+++ src/data.js
@@ -7,5 +7,9 @@ export const data = [
   {
 	 "alt": "An AI generated logo that contains extra content in the logo. The prompt was a logo for a plumbing business named A1 plumbing.",
 	"img": "aihorror2.webp",
+	},
+     {
+	 "alt": "An AI generated logo that adds extra text to the logo. The prompt was a logo for a construction company named A1 construction.",
+	"img": "aihorror2.webp",
 	}
  ]
